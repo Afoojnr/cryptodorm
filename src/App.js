628045e25484fc3adc68679a06c53a7ecc1f3913
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-04-29 20:43:04 +0200

    layout init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import { NavBar } from "./components";
+import { Layout, Typography, Space } from "antd";
 
 const App = () => {
   return (
-    <div>
-      <h1>CryptoDorm</h1>
+    <div className="app">
+      <div className="navbar">
+        <NavBar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
