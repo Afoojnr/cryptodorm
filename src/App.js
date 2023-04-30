6a9a944e4eeb621279db0a5ece9f968fe8a1bd10
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-04-30 22:14:45 +0200

    routes and setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
-import { NavBar } from "./components";
+import { Link, Route, Switch, Routes } from "react-router-dom";
+import {
+  NavBar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 import { Layout, Typography, Space } from "antd";
 import "./App.css";
 
@@ -10,8 +17,35 @@ const App = () => {
       <div className="navbar">
         <NavBar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white" }}>
+            CryptoDorm <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
