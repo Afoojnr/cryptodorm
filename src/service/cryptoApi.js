b1eb1249e3befab4626195acd1d422a39350dd41
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-05-07 21:42:40 +0200

    service

---
+++ src/service/cryptoApi.js
@@ -0,0 +1,19 @@
+
+
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     "tiers[0]": "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "0452ff9dfdmsh47bc902c40e9fc4p1e78bdjsna9271aba74d2",
+//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//   },
+// };
