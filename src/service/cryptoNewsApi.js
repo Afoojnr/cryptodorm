1cc4f90c473d765bdd4104eea109f84675415cfd
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-05-09 20:55:42 +0200

    News

---
+++ src/service/cryptoNewsApi.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "0452ff9dfdmsh47bc902c40e9fc4p1e78bdjsna9271aba74d2",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+const createRequest = (url) => ({
+  url,
+  headers: cryptoNewsHeaders,
+  //   params: params,
+});
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
+
