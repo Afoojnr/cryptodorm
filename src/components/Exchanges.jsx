4c602c87bffcfacfdabef5360eba9592c6d720a8
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-05-11 22:05:50 +0200

    exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import millify from "millify";
+import { Link } from "react-router-dom";
 import { Collapse, Row, Col, Typography, Avatar } from "antd";
 import HTMLReactParser from "html-react-parser";
 
@@ -10,7 +11,7 @@ const { Text } = Typography;
 const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const simplified = false
+  const simplified = false;
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState();
@@ -25,40 +26,50 @@ const Exchanges = () => {
 
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
-
+  console.log(cryptos);
   if (isFetching) return <Loader />;
 
   return (
     <>
       <Row>
         <Col span={6}>Coins</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
+        <Col span={6}>Price</Col>
+        <Col span={6}>Market Cap</Col>
         <Col span={6}>Change</Col>
       </Row>
       <Row>
-        {cryptos.map((crypto) => (
+        {cryptos?.map((crypto) => (
           <Col span={24}>
-            <Collapse>
-              <Panel
-                key={crypto.uuid}
-                showArrow={false}
-                header={(
-                  <Row key={crypto.uuid}>
-                    <Col span={6}>
-                      <Text><strong>{crypto.rank}.</strong></Text>
-                      <Avatar className="exchange-image" src={crypto.iconUrl} />
-                      <Text><strong>{crypto.name}</strong></Text>
-                    </Col>
-                    <Col span={6}>${millify(crypto.price)}</Col>
-                    <Col span={6}>{millify(crypto.markertCap)}</Col>
-                    <Col span={6}>{millify(crypto.change)}%</Col>
-                  </Row>
-                  )}
-              >
-                {HTMLReactParser(crypto.description || '')}
-              </Panel>
-            </Collapse>
+            <Link key={crypto.uuid} to={`/crypto/${crypto.uuid}`}>
+              <Collapse>
+                <Panel
+                  key={crypto.uuid}
+                  showArrow={false}
+                  header={
+                    <Row key={crypto.uuid}>
+                      <Col span={6}>
+                        <Text>
+                          <strong>{crypto.rank}.</strong>
+                        </Text>
+                        <Avatar
+                          size="small"
+                          className="exchange-image"
+                          src={crypto.iconUrl}
+                        />
+                        <Text>
+                          <strong>{crypto.name}</strong>
+                        </Text>
+                      </Col>
+                      <Col span={6}>${millify(crypto.price)}</Col>
+                      <Col span={6}>{millify(crypto.marketCap)}</Col>
+                      <Col span={6}>{millify(crypto.change)}%</Col>
+                    </Row>
+                  }
+                >
+                  {HTMLReactParser(crypto.description || "")}
+                </Panel>
+              </Collapse>
+            </Link>
           </Col>
         ))}
       </Row>
@@ -67,3 +78,37 @@ const Exchanges = () => {
 };
 
 export default Exchanges;
+
+// const dataSource = [
+//   {
+//     key: crypto.uuid,
+//     name: crypto.name,
+//     age: 32,
+//     address: "10 Downing Street",
+//   },
+// ];
+
+// const columns = [
+//   {
+//     title: "Coins",
+//     dataIndex: "coin",
+//     key: "coin",
+//   },
+//   {
+//     title: "Price",
+//     dataIndex: "price",
+//     key: "price",
+//   },
+//   {
+//     title: "Market Cap",
+//     dataIndex: "market_cap",
+//     key: "market_cap",
+//   },
+//   {
+//     title: "Daily Change",
+//     dataIndex: "daily_change",
+//     key: "daily_change",
+//   },
+// ];
+
+// <Table dataSource={dataSource} columns={columns} />;
